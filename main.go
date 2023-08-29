b56168a8626752efc331c611b81f6b2ee98dc586
AzureAD\고명민 <miung222@gmail.com>
2023-08-29 23:07:50 +0900

    websocket Upgrader, goroutine 관련 학습 주석 추가

--- main.go
+++ main.go
@@ -33,12 +33,15 @@ func main() {
 	}
 }
 
+// WebSocket 업그레이드 과정은 클라이언트가 HTTP 연결을 WebSocket 프로토콜로 업그레이드하도록 요청
+// ReadBufferSize 및 WriteBufferSize 필드는 들어오고 나가는 데이터의 버퍼 크기를 관리하여 WebSocket 통신 중 성능과 메모리 사용을 최적화하는 역할
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
 func socketHandler(w http.ResponseWriter, r *http.Request) {
+	// HTTP 연결을 WebSocket 프로토콜로 업그레이드
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Printf("WebSocket upgrade error: %s\n", err.Error())
@@ -53,9 +56,13 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 
 	openSocket := false
 
+	// Channel은 채널 연산자인 <- 을 통해 값을 주고 받을 수 있는 하나의 분리된 통로
+	// Channel은 map과 slice처럼 사용하기 전에 생성
+	// 전송과 수신은 다른 한 쪽이 준비될 때까지 block 상태
+	// 명시적인 lock이나 조건 변수 없이 goroutine이 synchronous하게 작업될 수 있도록한다.
 	eventCh := make(chan string)
 
-	// Start a goroutine to handle events asynchronously
+	// 이벤트를 비동기적으로 처리하기 위해 goroutine 실행
 	go func() {
 		for {
 			_, event, err := conn.ReadMessage()
@@ -63,11 +70,15 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 				fmt.Printf("WebSocket error: %s\n", err.Error())
 				return
 			}
+			// 채널 eventCh에 이벤트를 전송
 			eventCh <- string(event)
 		}
 	}()
 
 	for {
+		// select: goroutine이 다중 커뮤니케이션 연산에서 대기할 수 있게 한다.
+		// case들 중 하나가 실행될 때까지 block
+		// 다수의 case가 준비되는 경우에는 select가 무작위로 하나를 선택
 		select {
 		case <-ticker.C:
 			if openSocket {
@@ -79,6 +90,7 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 					return
 				}
 			}
+		// goroutine에서 비동기적으로 이벤트를 수신하였을 때
 		case event := <-eventCh:
 			if !openSocket {
 				openSocket = true
@@ -89,6 +101,9 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 					openSocket = false
 				}
 			}
+		// 다른 case들이 모두 준비되지 않았을 때 실행
+		default:
+			fmt.Printf("Select case not ready\n")
 		}
 	}
 }
